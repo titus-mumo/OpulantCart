4f49530b2beb28b831d14133445b4d773ac6db4d
titus-mumo <tituskennedy74@gmail.com>
2023-10-06 20:05:34 +0300

    just code

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useCart } from '../context/CartContext'
 
 export const CartCard = ({ product }) => {
     const format = Intl.NumberFormat('en-US')
     const { name, price, image } = product
     const { removeFromCart } = useCart()
+    const [quantity, setQuantity] = useState(1)
   return (
       <div className="mx-10 my-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex justify-normal flex-wrap align-middle">
           <div className=' mx-4 basis-1/6 p-1 rounded-t-lg'>
@@ -13,18 +14,21 @@ export const CartCard = ({ product }) => {
             </a>
           </div>
 
-          <div className="p-2 basis-1/2 flex justify-between flex-wrap align-middle">
-              <div className= "flex justify-around align-middle h-full">
-                    <a href="/" className='basis-1/2'>
-                        <h5 className="mb-2 text-xl tracking-tight text-gray-900 dark:text-white">{ name }</h5>
+          <div className="p-2 flex justify-start flex-wrap align-middle">
+              <div className= "flex justify-start align-middle h-full">
+                    <a href="/" className='mx-6'>
+                        <h5 className="mb-2 text-md tracking-tight text-gray-900 dark:text-white">{ name }</h5>
                     </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 basis-1/2">Kshs. { format.format(price)}</p>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 mx-6">Kshs. { format.format(price)}</p>
+                    <input type='number' value={quantity} onChange={(e) => setQuantity(e.target.value < 1? quantity:e.target.value)}></input>
               </div>
-              <div>
-                    <span onClick={() => removeFromCart(product)} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-500 rounded-lg hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 :bg-red-400 dark:hover:bg-red-500 dark:focus:ring-red-600">
+              <div className='text-black dark:text-white mx-6'> Sub-Total: {price * quantity}</div>
+              <div className='mx-6'>
+                    <span onClick={() => removeFromCart(product)} className="hover:cursor-pointer inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-500 rounded-lg hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 :bg-red-400 dark:hover:bg-red-500 dark:focus:ring-red-600">
                         Remove
                     </span>
               </div>
+              
             </div>
         </div>
   )
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -46,10 +46,10 @@ export const ProductCard = ({ product }) => {
           <div className="flex items-center justify-between">
           <span className="text-lg text-gray-900 dark:text-white">Kshs. {format.format(price)}</span>
           {
-            isInCart ?                     <span onClick={() => removeFromCart(product)} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-500 rounded-lg hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 :bg-red-400 dark:hover:bg-red-500 dark:focus:ring-red-600">
+            isInCart ?                     <span onClick={() => removeFromCart(product)} className="hover:cursor-pointer inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-500 rounded-lg hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 :bg-red-400 dark:hover:bg-red-500 dark:focus:ring-red-600">
                         Remove
                     </span>:
-              <span className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={() => handleAddToCart(product)}>Add to cart</span>
+              <span className="hover:cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={() => handleAddToCart(product)}>Add to cart</span>
           }
             
           </div>
